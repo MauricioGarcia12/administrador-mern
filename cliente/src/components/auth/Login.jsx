5656b1d132640a73e6033e65264ade3c2d4ce58d
maumainsaw <maumainsaw@gmail.com>
2021-02-07 21:17:12 -0600

    Creando paginas Login y Crear Cuenta

--- cliente/src/components/auth/Login.jsx
+++ cliente/src/components/auth/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import {Link} from 'react-router-dom'
 const Login = () => {
+
+    //state para iniciar sesion
+    const [usuario, guardarUsuario] = useState({
+        email:'',
+        password:''
+    });
+
+    //extraer de usuario
+    const {email, password} = usuario;
+    const onChange= e =>{
+        guardarUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //cuando sel user quiere iniciar sesion
+    const onSubmit = e =>{
+        e.preventDefault();
+        
+    }
     return (
-        <div>
-            Login
+        <div className="form-usuario">
+            <div className="contenedor-form sombra'dark">
+                <h1>Iniciar Sesion</h1>
+
+                <form onSubmit={onSubmit}>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Tu email"
+                        value={email}
+                        onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Email</label>
+                        <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu password"
+                        value={password}
+                        onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                        type="submit" 
+                        className="btn btn-block btn-primario"
+                        value="Iniciar Sesion"
+                        />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener cuenta
+                </Link>
+            </div>
         </div>
     )
 }
