a30fbd742ca3dffe34095a126774e2eb69ef7321
maumainsaw <maumainsaw@gmail.com>
2021-02-08 09:38:03 -0600

    Listando proyectos

---
+++ cliente/src/components/proyectos/ListadoProyectos.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Proyecto from './Proyecto'
+
+const ListadoProyectos = () => {
+    const proyectos = [
+        {nombre:'tienda Virtual'},
+        {nombre:'Sitio Web Design'}
+    ]
+    return (
+        <ul className='listado-proyectos'>
+            {proyectos.map(proyecto =>(
+                <Proyecto proyecto={proyecto}/>
+            ))}
+        </ul>
+    )
+}
+
+export default ListadoProyectos
--- cliente/src/components/proyectos/NuevoProyecto.jsx
+++ cliente/src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const NuevoProyecto = () => {
+
+    //State para proyecto
+    const [proyecto, guardarProyecto] = useState({
+        nombre:''
+    });
+
+    const {nombre} =proyecto
+    //leee contenidos del input
+    const onCheageProyecto = e=>{
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //cuando el usuario envia un proyecto
+    const onSubmitProyecto = e=>{
+        e.preventDefault();
+
+        //validar el proyecto
+
+        //agregar al state
+
+        //reiniciar el form
+    }
     return (
         <>
             <button
@@ -12,16 +37,19 @@ const NuevoProyecto = () => {
 
             <form action="" 
             className='formulario-nuevo-proyecto'
+            onSubmit={onSubmitProyecto}
             >
                 <input 
                 type="text" 
                 className='input-text'
                 placeholder='Nombre Proyecto'
                 name='nombre'
+                onChange={onCheageProyecto}
+                value={nombre}
                 />
 
                 <input 
-                type="text"
+                type="submit"
                 className='btn btn-primario btn-block'
                 value='Agregar Proyecto'
                 />
